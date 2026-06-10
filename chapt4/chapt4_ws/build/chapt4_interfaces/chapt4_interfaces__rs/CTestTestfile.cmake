0ebd633a55e5332b091eb76e412f2176deeb5c5f
Dfcchh <2640128146@qq.com>
2026-06-10 16:07:32 +0800

    移除chapt4，放到新仓库

--- chapt4/chapt4_ws/build/chapt4_interfaces/chapt4_interfaces__rs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dfc/chapt4/chapt4_ws/build/chapt4_interfaces/chapt4_interfaces__rs
-# Build directory: /home/dfc/chapt4/chapt4_ws/build/chapt4_interfaces/chapt4_interfaces__rs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
